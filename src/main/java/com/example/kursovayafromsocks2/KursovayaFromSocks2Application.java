5cbb7f88b74f83a1a0955aa31d946da7a234e02e
user <yulechkaakopyan@yandex.ru>
2023-03-09 23:11:21 +0300

    Основная папка

---
+++ src/main/java/com/example/kursovayafromsocks2/KursovayaFromSocks2Application.java
@@ -0,0 +1,13 @@
+package com.example.kursovayafromsocks2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovayaFromSocks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursovayaFromSocks2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kursovayafromsocks2/KursovayaFromSocks2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursovayafromsocks2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovayaFromSocks2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
